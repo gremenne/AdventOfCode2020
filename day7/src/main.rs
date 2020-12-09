2b365d4c6b0f16d716850f821538a94090b93a9c
Gregory Lumen <glumen@blueorigin.com>
2020-12-08 16:43:19 -0800

    Day 7 star 1

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -4,15 +4,16 @@ use std::io::{self, BufRead};
 use std::path::Path;
 use regex::Regex;
 use std::collections::HashMap;
+use std::collections::HashSet;
 
-struct Bag<'a> {
+struct Bag {
     bag_name: String,
-    contains: Vec<(u32, &'a Bag<'a>)>,
-    contained_in: Vec<&'a Bag<'a>>
+    contains: Vec<(u32, String)>,
+    contained_in: Vec<String>
 }
 
-impl<'a> Bag<'a> {
-    fn new(bag_name: String ) -> Bag<'a> {
+impl Bag {
+    fn new(bag_name: String ) -> Bag {
         Bag{ bag_name: bag_name,
             contains: Vec::new(),
             contained_in: Vec::new(),
@@ -20,38 +21,52 @@ impl<'a> Bag<'a> {
     }
 }
 
-struct BagTracker<'a> {
-    bag_map: HashMap<String, Bag<'a>>
+struct BagTracker {
+    bag_map: HashMap<String, Bag>,
 }
 
-impl<'a> BagTracker<'a> {
-    fn new() -> BagTracker<'a> {
+impl BagTracker {
+    fn new() -> BagTracker {
         BagTracker{ bag_map: HashMap::new() }
     }
 
     fn add_bag(&mut self, bag_name: &String)
     {
-        self.bag_map.insert(
-            bag_name.to_string(),
-            Bag::new(bag_name.to_string()));
-    }
-
-    fn get_bag_or_add<'b>(&'b mut self, bag_name: &String) -> &'b mut Bag<'a>
-    {
         if !self.bag_map.contains_key(bag_name) {
-            self.add_bag(bag_name);
+            self.bag_map.insert(
+                bag_name.to_string(),
+                Bag::new(bag_name.to_string()));
         }
-
-        self.bag_map.get_mut(bag_name).unwrap()
     }
 
-    fn add_bag_relationship<'b>(&'b mut self, src_bag_name: &String, dst_bag_name: &String, quantity: u32)
+    fn add_bag_relationship(&mut self, src_bag_name: &String, dst_bag_name: &String, quantity: u32)
     {
-        let src_bag = self.get_bag_or_add(src_bag_name);
-        let dst_bag = self.get_bag_or_add(dst_bag_name);
+        self.add_bag(src_bag_name);
+        self.add_bag(dst_bag_name);
 
-        src_bag.contains.push((quantity, dst_bag));
-        dst_bag.contained_in.push(src_bag);
+        self.bag_map.get_mut(src_bag_name).unwrap().contains.push((quantity, dst_bag_name.to_string()));
+        self.bag_map.get_mut(dst_bag_name).unwrap().contained_in.push(src_bag_name.to_string());
+    }
+
+    fn get_bag(&self, bag_name: &String) -> &Bag
+    {
+        &self.bag_map[bag_name]
+    }
+
+    fn get_parents(&self, bag_name: &String) -> Vec<&Bag> {
+        self.get_bag(bag_name).contained_in.iter().map(|x| self.get_bag(x)).collect()
+    }
+
+    fn get_all_parents(&self, bag_name: &String) -> Vec <&Bag> {
+        self.get_parents(bag_name)
+            .iter()
+            .map(|x| *x )
+            .chain(
+                self.get_parents(bag_name)
+                .iter()
+                .map(|x|self.get_all_parents(&x.bag_name))
+                .flatten())
+            .collect()
     }
 }
 
@@ -94,6 +109,10 @@ fn main() {
     for line in lines {
         parse_bag_rule(&line, &mut bags);
     }
+
+    let parents : HashSet<_> = bags.get_all_parents(&"shiny gold".to_string()).into_iter().map(|x| x.bag_name.to_string()).collect();
+
+    println!("{}", parents.len());
 
 }
 
